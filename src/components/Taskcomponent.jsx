0d94149f95134e04109c98b9dc5f773ea021434a
shalini <s.rathore43@gmail.com>
2025-05-07 12:27:19 +0530

    sasa

--- src/components/Taskcomponent.jsx
+++ src/components/Taskcomponent.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-
 export default function Taskcomponent() {
     const [taskname,settaskname] = useState("")
     const [taskui,settaskui]= useState()
@@ -7,20 +6,49 @@ export default function Taskcomponent() {
     const [Tasks,setTasks]= useState([])
     let addTask = ()=>
     {
-        let t = {name:taskname,status:'Incomplete'}
+        if((Tasks.findIndex((t)=>t.name==taskname))!=(-1))
+        {
+            alert("already present")
+            return
+        }
+        let len = Tasks.length;
+        let id1 = 1;
+        if(Tasks.length>0)
+        {
+            id1=(Tasks[len-1].id)+1        
+        }      
+        let t = {id:id1,name:taskname,status:'Incomplete'}
         Tasks.push(t)
         showTask()
-
     }
+    let deleteTask=(id)=>
+    {
+        let index = Tasks.findIndex((t)=>t.id ==id )
+        Tasks.splice(index,1)
+        showTask()
+    }
+    let updateTask=(id)=>
+        {
+            let index = Tasks.findIndex((t)=>t.id ==id )
+            Tasks[index].status="Complete"
+            showTask()
+        }
     let showTask = ()=>
     {
         let tempary =  Tasks.map((t)=>{
-        return <tr>
+            let cname = "orange"
+            if(t.status == "Complete")
+            {cname="yellowgreen"}
+        return <tr className='text-center text-black' style={{backgroundColor:cname}}>
+            <td className='border'>{t.id}</td>
             <td className='border'>{t.name}</td>
             <td className='border'>{t.status}</td>
-            <td className='border'><input type="button" value="Delete"  //add a deleteitem(id)on click
+            <td className='border'><input type="button" value="Delete"
+            onClick={()=>deleteTask(t.id)}  //add a deleteitem(id)on click
             className='bg-red-600 text-white' /></td>
-            <td className='border'><input type="button" value="Complete" className='bg-green-600 text-white' /></td>
+            <td className='border'><input type="button" 
+              onClick={()=>updateTask(t.id)} 
+              value="Complete" className='bg-green-600 text-white' /></td>
             </tr>
         })
         settaskui(tempary)
@@ -41,7 +69,9 @@ export default function Taskcomponent() {
         </div>
 <div className='text-red-500 '> 
     <table className='w-full'>
-        <thead className='w-full'><tr ><th className='border'>Task</th>
+        <thead className='w-full'><tr >
+        <th className='border'>Id</th>
+            <th className='border'>Task</th>
          <th className='border'>Status</th>
           <th className='border'>Delete</th>
            <th className='border'>Update</th></tr></thead>
